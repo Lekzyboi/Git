daf42de7393dd971ee6918de0f794a365dca583a
Your Nameclear <you@example.com>
2022-12-18 13:36:45 -0300

    foods added

--- Hungry.py
+++ Hungry.py
@@ -1,4 +1,11 @@
 print("I'm Hungry")
 print('how is it going')
 print('world Cup Final')
-#%%
+
+food = input('What is for Breakfast?')
+if input == 'Rice':
+    print('Nice Breakfast')
+elif input == 'Beans':
+    print('Not too good')
+else:
+    print('Why have you not eaten')
